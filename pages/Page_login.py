b251c1c46b28a2ec17c60fd523eeb2a8ec114b5d
coder <dnstylish@gmail.com>
2022-04-28 00:03:08 +0700

    Initial commit

---
+++ pages/Page_login.py
@@ -0,0 +1,14 @@
+def txt_username():
+    return "//*[@id=\"User\"]"
+
+
+def txt_password():
+    return "//*[@id=\"Password\"]"
+
+
+def btn_login():
+    return "//*[@id=\"Lnew1\"]"
+
+
+def msg_result():
+    return "//*[@id=\"Ltbao\"]"
